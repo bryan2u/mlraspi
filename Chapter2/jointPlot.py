7a3ce2b60166c57388015bdc408d54d18b6e0807
bryan2u <56008129+bryan2u@users.noreply.github.com>
2020-12-19 16:08:06 +0800

    Update jointPlot.py

--- Chapter2/jointPlot.py
+++ Chapter2/jointPlot.py
@@ -4,6 +4,6 @@ import seaborn as sns
 # Load the Iris dataset
 iris = sns.load_dataset("iris")
 # Generate the scatter plot
-sns.jointplot(x="sepal_length",y="sepal_width", data=iris,size=6)
+sns.jointplot(x="sepal_length",y="sepal_width", data=iris,height=6)
 # Display the plots
 plt.show()
