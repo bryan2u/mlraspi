a74f8f886a9cb0b655176ab86e4ac66069ede177
bryan2u <56008129+bryan2u@users.noreply.github.com>
2020-12-19 16:15:13 +0800

    Update pairPlot.py

--- Chapter2/pairPlot.py
+++ Chapter2/pairPlot.py
@@ -4,6 +4,6 @@ import seaborn as sns
 # Load the Iris dataset
 iris = sns.load_dataset("iris")
 # Generate the pair plots
-sns.pairplot(iris, hue="species", size=2.5)
+sns.pairplot(iris, hue="species", height=2.5)
 # Display the plots
 plt.show()
