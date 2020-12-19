f7851b1ec430a40381cf9b131847a118be5ddd45
bryan2u <56008129+bryan2u@users.noreply.github.com>
2020-12-19 16:12:55 +0800

    Update kdePlot.py

--- Chapter2/kdePlot.py
+++ Chapter2/kdePlot.py
@@ -4,7 +4,7 @@ import seaborn as sns
 # Load the Iris dataset
 iris = sns.load_dataset("iris")
 # Generate the kde plot
-sns.FacetGrid(iris,hue="species",size=6) \
+sns.FacetGrid(iris,hue="species",height=6) \
 .map(sns.kdeplot,"sepal_length") \
 .add_legend()
 # Display the plot
