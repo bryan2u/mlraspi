9d67691867f5b93bdcb0f885665ffcbbe10cb7ae
bryan2u <56008129+bryan2u@users.noreply.github.com>
2020-12-23 01:39:37 +0800

    Update kerasFashionTest.py

--- Chapter6/kerasFashionTest.py
+++ Chapter6/kerasFashionTest.py
@@ -8,7 +8,8 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
-K.set_image_dim_ordering('th')
+# K.set_image_dim_ordering('th')
+K.set_image_data_format('channels_last')
 # Set a random seed
 seed = 42
 np.random.seed(seed)
@@ -17,8 +18,11 @@ np.random.seed(seed)
 # Flatten all of the 28 x 28 images into 784 element numpy input
 # data vectors.
 pixelNum = train_images.shape[1] * train_images.shape[2]
-train_images = train_images.reshape(train_images.shape[0],1,28,28).astype('float32')
-test_images = test_images.reshape(test_images.shape[0],1,28,28).astype('float32')
+# train_images = train_images.reshape(train_images.shape[0],1,28,28).astype('float32')
+# test_images = test_images.reshape(test_images.shape[0],1,28,28).astype('float32')
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+
 # Normalize inputs from 0-255 to 0-1
 train_images = train_images / 255.0
 test_images = test_images / 255.0
@@ -30,7 +34,7 @@ numClass = test_labels.shape[1]
 def cnn_model():
     # create model
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation="relu"))
+    model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
