702f100233bffe59e284391675d095c376e64946
bryan2u <56008129+bryan2u@users.noreply.github.com>
2020-12-23 02:31:09 +0800

    Update jasonTest.py

--- Chapter6/jasonTest.py
+++ Chapter6/jasonTest.py
@@ -59,7 +59,7 @@ def evaluate_model(dataX, dataY, n_folds=5):
         history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
         # evaluate model
         _, acc = model.evaluate(testX, testY, verbose=0)
-        print('> %.3f' % (acc ∗ 100.0))
+        print('> %.3f' % (acc * 100.0))
         # append scores
         scores.append(acc)
         histories.append(history)
@@ -81,7 +81,7 @@ def summarize_diagnostics(histories):
 # summarize model performance
 def summarize_performance(scores):
     # print summary
-    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)∗100, std(scores)∗100, len(scores)))
+    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
     # box and whisker plots of results
     pyplot.boxplot(scores)
     pyplot.show()
